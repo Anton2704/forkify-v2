5f56c5f2776dae435930fc9d81c18d25c227d4b7
Anton27 <antonkleymenov27@yandex.ru>
2020-12-11 10:44:44 +0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a750540d-1320-48b9-b9f6-96d053e40c66';
+export const MODAL_CLOSE_SEC = 2.5;
